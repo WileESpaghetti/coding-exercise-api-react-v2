ce65df2c1262e607dc0a9aa6409ee56dbbc4c8b9
Lehman Black <WileESpaghetti@users.noreply.github.com>
2020-06-28 20:20:22 -0500

    handle click events in CsvUpload component extra time taken to find out if React had an alternative to getElementById and learning about refs

--- src/CsvUpload.js
+++ src/CsvUpload.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import { Input } from 'semantic-ui-react'
+import { Button, Input } from 'semantic-ui-react'
 
 class CsvUpload extends Component {
     constructor(props) {
         super(props);
+        this.myRef = React.createRef();
+        this.handleClick = this.handleClick.bind(this);
+        this.getFileInputNode = this.getFileInputNode.bind(this);
         this.state = {};
+    }
+    getFileInputNode() {
+        return this.myRef.current.inputRef.current;
+    }
+
+    handleClick() {
+        let files = this.getFileInputNode().files;
+
+        if (files.length > 0) {
+            console.log(files[0]);
+        }
     }
     render() {
         return (
-            <Input type="file" id="csv-upload" accept=".csv"/>
+            <Input type="file" accept=".csv" ref={this.myRef} action={<Button id="do-upload-csv" onClick={this.handleClick}>Upload</Button>}/>
         );
     }
 }
